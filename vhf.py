666e781bce20ac763dd873a8b5ad2acc64512ef8
thejerrytan <jerrytansk@gmail.com>
2016-09-18 02:52:42 +0800

    dfs, cycle detection to decide when to backtrack to a new unexplored branch

--- vhf.py
+++ vhf.py
@@ -5,30 +5,35 @@ import numpy.linalg as la
 
 X_DIM            = 30
 Y_DIM            = 30
+NUM_SENSORS      = 4
+N_CYCLE          = 6
 dest             = np.array([25, 25])
 src              = (0,0)
 state            = [np.array([0,0]), 0] # X,Y,bearing where bearing is UP (0), DOWN (180), LEFT (270), RIGHT (90)
 agent_map        = [] # global map reflecting agent's current knowledge of the map, 1 is unexplored, -1 is visited, -2 is obstacle, 2 is no obstacle
 obstacle_field   = []
-obstacle_density = 0.2
+obstacle_density = 0.3
 sensor           = ['front', 'left', 'right', 'back']
 moves_x          = [0]
 moves_y          = [0]
+dfs_branches     = []
+branches         = {}
 backtrack_penalty = -1
+curr_dest        = dest
 
 def update_map(loc, marker):
 	global agent_map
 	if marker == 'obstacle':
 		value = -2
 	elif marker == 'visited':
-		value = -1
+		value = backtrack_penalty
 	elif marker == 'free':
 		value = 2
 	else:
 		value = 0
 	agent_map[loc[0], loc[1]] = value
 
-def plotter(ax, x, y, params):
+def plotter(ax):
 	ax.cla()
 	ax.set_xticks(np.arange(0,30,1))
 	ax.set_yticks(np.arange(0,30,1))
@@ -37,10 +42,16 @@ def plotter(ax, x, y, params):
 	ax.scatter(moves_x[:-1], moves_y[:-1], **{'c':'r', 's':40, 'marker':"x"})
 	ax.scatter(moves_x[-1:], moves_y[-1:], **{'c':'r', 's':40, 'marker':"o"})
 	indices = np.where(agent_map==-2)
+	ax.scatter(indices[0], indices[1], **{'marker':"*"})
+	indices = np.where(agent_map==2)
 	ax.scatter(indices[0], indices[1], **{'marker':"+"})
+	# plt.pause(1)
 	x = raw_input("Press [Enter] to continue\n")
 	if x == 'q':
 		sys.exit(1)
+
+def within_map(x, y):
+	return (x >= 0) and (x < X_DIM) and (y >= 0) and (y < Y_DIM)
 
 def calculate_next_move():
 	""" 
@@ -58,10 +69,29 @@ def calculate_next_move():
 		obs_density = obs_sum / slice_.shape[0]
 		return obs_density
 
-	def calc_penalty():
-		pass
+	def calc_penalty(slice_):
+		if not isinstance(slice_, np.ndarray):
+			slice_ = np.array([slice_])
+		else:
+			if len(slice_) == 0:
+				return 0.0
+		return np.ma.average(slice_)
 
-	direction = (dest - state[0]) / la.norm(dest - state[0])
+	def look_ahead(x, y):
+		""" count number of spaces reachable in new state"""
+		free_config = 0
+		if within_map(x+1, y) and (agent_map[x+1,y] != -2):
+			free_config += 1
+		if within_map(x, y+1) and (agent_map[x,y+1] != -2):
+			free_config += 1
+		if within_map(x-1, y) and (agent_map[x-1,y] != -2):
+			free_config += 1
+		if within_map(x, y-1) and (agent_map[x, y-1] != -2):
+			free_config += 1
+		return (free_config - 1) / 2 # Do not count position where agent came from
+
+	direction = (curr_dest - state[0]) / la.norm(curr_dest - state[0])
+	# print(direction)
 	dots = []
 	dist_to_dir = {}
 	# alpha = np.e * (1 - 1 / np.e)
@@ -70,7 +100,9 @@ def calculate_next_move():
 		for j in range(-1,2): # Y
 			# Calculate max. dot product with direction vector
 			# If within map
-			if state[0][0] + i >= 0 and state[0][0] + i < 30 and state[0][1] + j >= 0 and state[0][1] + j < 30:
+			new_i = state[0][0] + i
+			new_j = state[0][1] + j
+			if new_i >= 0 and new_i < 30 and new_j >= 0 and new_j < 30:
 				# Exclude diagonal moves
 				if (i == 0 or j == 0) and (not (i == 0 and j == 0)):
 					is_obs = (agent_map[state[0][0]+i, state[0][1]+j] == -2)
@@ -79,48 +111,78 @@ def calculate_next_move():
 					try:
 						if i == -1 and j == 0:
 							slice_ = agent_map[0:x, y]
-							obs_density = calc_obs_density(slice_)
+							obs_density = calc_penalty(slice_)
 						elif i == 0 and j == 1:
 							slice_ = agent_map[x, y+1:]
-							obs_density = calc_obs_density(slice_)
+							obs_density = calc_penalty(slice_)
 						elif i == 1 and j == 0:
 							slice_ = agent_map[x+1:, y]
-							obs_density = calc_obs_density(slice_)
+							obs_density = calc_penalty(slice_)
 						elif i == 0 and j == -1:
 							slice_ = agent_map[x, 0:y]
-							obs_density = calc_obs_density(slice_)
+							obs_density = calc_penalty(slice_)
 						else:
 							pass
 					except Exception as e:
 						print e
 						obs_density = 0.0
 					obs_density = np.nan_to_num(obs_density)
+					free_config  = look_ahead(new_i, new_j)
 					move_vec = np.array([i,j])
 					if is_obs:
 						product = -1 * np.inf
 						# product = np.dot(move_vec, direction) - alpha * (obs_density + np.inf)
 					else:
-						product = np.dot(move_vec, direction) + agent_map[state[0][0] + move_vec[0], state[0][1] + move_vec[1]]
+						# product = np.dot(move_vec, direction) + agent_map[state[0][0] + move_vec[0], state[0][1] + move_vec[1]]
+						product = np.dot(move_vec, direction) + obs_density + free_config
 						# product = np.dot(move_vec, direction) - alpha * (obs_density)
-					print(i, j, product)
+					# print(i, j, product)
 					dots.append(product)
 					dist_to_dir[product] = np.array([i,j])
 	if len(dots) == 0:
 		print("No legal move!")
 	else:
+		if len(filter(lambda x: x!=-np.inf, dots)) > 1:
+			try:
+				added = branches[str(state[0][0])+str(state[0][1])]
+			except KeyError as e:
+				dfs_branches.append(state[0]) # Save location of branching to come back later
+				branches[str(state[0][0])+str(state[0][1])] = 1
+			print("Branches: %d" % len(dfs_branches))
 		# print(dots)
-		pass
 	return dist_to_dir[max(dots)]
+
+def detect_cycle(n):
+	"""n is int referring to length of cycle (number of steps)"""
+	if len(moves_x) < n+1:
+		return False
+	else:
+		origin = (moves_x[-n-1], moves_y[-n-1])
+		end    = (moves_x[-1], moves_y[-1])
+		if origin == end:
+			return True
+		else:
+			return False
 
 def bearing_to_vec(bearing):
 	"""Given bearing in degrees, return a unit vec (x,y) in that direction"""
 	return np.array([np.cos((np.pi / 180) * bearing - np.pi / 2), np.sin((np.pi / 180) * bearing + np.pi / 2)])
 
-def detect_obstacle():
-	"""Obstacle is reported relative to agent's bearing. Can only be one of FRONT, LEFT, RIGHT, BACK"""
+def vec_to_bearing(vec, bearing):
+	"""Given unit vector and agent's current bearing, return direction to turn in degrees"""
+	pass
+
+def detect_obstacle(direction='front', mode='random'):
+	"""
+		Two modes of operation - random(ly) senses a random direction while directed senses in direction.
+		Obstacle is reported relative to agent's bearing. Can only be one of FRONT, LEFT, RIGHT, BACK
+	"""
 	global state
-	sense_dir = sensor[np.random.choice(3, 1)[0]]
 	bearing = state[1]
+	if mode == 'random':
+		sense_dir = sensor[np.random.choice(3, 1)[0]]
+	else:
+		sense_dir = direction
 	if sense_dir == 'front':
 		loc = state[0] + bearing_to_vec(bearing)
 	elif sense_dir == 'left':
@@ -128,12 +190,13 @@ def detect_obstacle():
 	elif sense_dir == 'back':
 		loc = state[0] + bearing_to_vec((bearing + 180) % 360)
 	else:
+		# right
 		loc = state[0] + bearing_to_vec((bearing + 90) % 360)
 	is_obstacle = (obstacle_field[loc[0], loc[1]]==1)
-	if is_obstacle: print("Obstacle [%d, %d, %s]" % (state[0][0], state[0][1], sense_dir))
+	# if is_obstacle: print("Obstacle [%d, %d, %s]" % (state[0][0], state[0][1], sense_dir))
 	return (loc, is_obstacle)
 
-def move(coord):
+def move(coord, backtrack=False):
 	"""Agent must turn to face the right bearing before moving forward by 1 step, coord is vector of movement"""
 	global state
 	print("Move [%d, %d]" % (coord[0], coord[1]))
@@ -144,8 +207,9 @@ def move(coord):
 	else:
 		update_map(new_state, 'visited')
 		state[0] = new_state
-		moves_x.append(state[0][0])
-		moves_y.append(state[0][1])
+		if not backtrack:
+			moves_x.append(state[0][0])
+			moves_y.append(state[0][1])
 
 def init(ax1, ax2):
 	global obstacle_field, agent_map
@@ -157,13 +221,13 @@ def init(ax1, ax2):
 	ax2.set_xticks(np.arange(0,30,1))
 	ax2.set_yticks(np.arange(0,30,1))
 	indices = np.where(obstacle_field==1)
-	ax2.scatter(indices[0], indices[1], marker="+") # Orignal obstacle map
+	ax2.scatter(indices[0], indices[1], marker="*") # Orignal obstacle map
 	ax1.set_xticks(np.arange(0,30,1))
 	ax1.set_yticks(np.arange(0,30,1))
 	ax1.set_xlim(-1, 30)
 	ax1.set_ylim(-1, 30)
-	indices = np.where(agent_map==1)
-	ax1.scatter(indices[0], indices[1], marker="+") # Agent's map
+	indices = np.where(agent_map==-2)
+	ax1.scatter(indices[0], indices[1], marker="*") # Agent's map
 	# plt.imshow(obstacle_field, cmap='hot', interpolation='nearest')
 	ax1.grid() # Call this once as it toggles state 
 	ax2.grid()
@@ -171,21 +235,40 @@ def init(ax1, ax2):
 	plt.show()
 
 def main():
+	global curr_dest
 	np.random.seed()
 	fig, (ax1, ax2)  = plt.subplots(2,1, figsize=(17,8))
 	init(ax1, ax2)
 	win   = False
 	count = 0
 	while (not win):
-		(loc, is_obstacle) = detect_obstacle()
-		if is_obstacle:
-			update_map(loc, 'obstacle')
-		else:
-			update_map(loc, 'free')
-		plotter(ax1, moves_x[:-1], moves_y[:-1], {'c':'r', 's':40, 'marker':"x"})
+		for x in range(0, NUM_SENSORS):
+			(loc, is_obstacle) = detect_obstacle(direction=sensor[x], mode='directed')
+			if is_obstacle:
+				update_map(loc, 'obstacle')
+			else:
+				update_map(loc, 'free')
+		plotter(ax1)
 		coord = calculate_next_move()
 		move(coord)
 		count += 1
+		if detect_cycle(N_CYCLE):
+			try:
+				curr_dest = dfs_branches.pop()
+				print("Cycle detected, backtrack to last branch [%d, %d]" % (curr_dest[0], curr_dest[1]))
+				index = 1
+				while (state[0][0] != curr_dest[0]) or (state[0][1] != curr_dest[1]):
+					prev_x = moves_x[-1*index]
+					prev_y = moves_y[-1*index]
+					move(np.array([prev_x - state[0][0], prev_y - state[0][1]]), backtrack=False)
+					index += 2
+					plotter(ax1)
+					count+= 1
+			except IndexError as e:
+				print("Error! No last branch to backtrack to...There exists no path to the dest.")
+				sys.exit(1)
+			print("Reached last checkpoint [%d, %d]" % (curr_dest[0], curr_dest[1]))
+			curr_dest = dest
 		if state[0][0] == dest[0] and state[0][1] == dest[1]:
 			win = True
 		if count > 100: break
