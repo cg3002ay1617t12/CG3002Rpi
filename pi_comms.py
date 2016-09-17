8e3a7f683a2cb0eaacd53fa603f8b4c892fe89d4
Joanna <joannakxt@ymail.com>
2016-09-17 20:49:18 +0700

    parse data

--- pi_comms.py
+++ pi_comms.py
@@ -4,13 +4,14 @@ import time
 #enum {0, 1, 2, 3, 4 , 5, 6, 7, 8 }
 
 CurrMode = 0
+packet_type = 0 #save the type of packet
 
 booted = '0'
 bufferSize = 0
 
 ser = serial.Serial(
 		port ='/dev/ttyAMA0', 
-		baudrate = 57600,
+		baudrate = 115200,
 		timeout = 3
 )
 
@@ -21,16 +22,19 @@ packet_size_count = 0
 	  
 packet_seq = '0'
 
+dataSize[8] 
+data = 0 
 numComponent = 0 # MAX 10
 componentID = []
 componentFlag = '0'
 componentTemp = 0
 
-dataIndex = 0 
+dataIndex = 8 
 payloadData = 1 # need to findout why is it it bugging out
 
-crcCount = 4
-crcData = 1 # need to findout why is it it bugging out
+crcSize = 4
+crcIndex=0
+crcData = []# need to findout why is it it bugging out
 	  
 readStatus = False
 
@@ -41,6 +45,7 @@ def read():
 		while readStatus:
 
 			incomingByte = ser.read()
+
 			if CurrMode == 0: 
 				if incomingByte == 60:
 					CurrMode = 1
@@ -49,111 +54,92 @@ def read():
 			elif CurrMode == 1:
 				if incomingByte == 40:
 					print("Recieved ACK")
-					CurrMode = 2
+					packet_type = 1 
+					CurrMode = 3
 				elif incomingByte == 41:
 					print("Recieved NACK")
-					CurrMode = 2
+					packet_type = 2
+					CurrMode = 3
 				elif incomingByte == 42:
 					print("Recieved Prob")
-					CurrMode = 2
+					packet_type = 3
+					CurrMode = 3
 				elif incomingByte == 43:
 					print("Recieved Read")
-					CurrMode = 2
+					packet_type = 4
+					CurrMode = 3
 				elif incomingByte == 44:
 					print("Recieved Data")
-					CurrMode = 2
+					packet_type = 5
+					CurrMode = 3
 				elif incomingByte == 45:
 					print("Recieved Request")
-					CurrMode = 2
+					packet_type = 6
+					CurrMode = 3
 				else:
 					CurrMode = 8
 					print("CORRUPT")
-
-			elif CurrMode == 2:
-				print("Payload_length")
-				if packet_size_count == 0:
-					packet_size += (incomingByte-48) * 100
-					packet_size_count += 1
-				elif packet_size_count == 1:
-					packet_size += (incomingByte-48) * 10
-					packet_size_count += 1
-				else:
-					packet_size += (incomingByte-48)
-					packet_size_count = 0
-					CurrMode = 3
-					print("DEBUG!!!")
-					print(packet_size)
-					print("DEBUG!!!")
-					dataIndex = packet_size
 			
 			elif CurrMode == 3 :
 				print("Payload_seq")
-				if packet_seq == (incomingByte-48): 
+				if packet_seq == 0: 
 					CurrMode = 8
 					print("CORRUPT")
 				else: 
 					CurrMode = 4
 					packet_seq = not packet_seq
 		
-			elif CurrMode == COMPONENT_ID :
+			elif CurrMode == 4 :
 				print("component_id")
-				if componentFlag == '0': 
-					componentTemp += (incomingByte-48)*10
-					componentFlag = not componentFlag
-				else: 
-					componentTemp += (incomingByte-48)
-					componentID[numComponent] = componentTemp
-					numComponent += 1
-					componentTemp = 0
-					componentFlag = not componentFlag
-		
-				if numComponent == 11:
+				if incomingByte >0 and incomingByte <41: 
+					CurrMode = 5 
+				else:
 					CurrMode = 8
 					print("CORRUPT")
-		
-				elif incomingByte == 64:
-					if componentFlag == '1':
-						CurrMode = 8
-						print("CORRUPT")
-					CurrMode = 5
 		
 			elif CurrMode == 5 :
 				print("payload")
 				if dataIndex > -1: 
-					payloadData = payloadData + pow(10.0,dataIndex-1) * (incomingByte-48)
-					dataIndex = dataIndex - 1
+					data = data + incomingByte
+					dataIndex = dataIndex-1 
 					if dataIndex == 0:
 						CurrMode = 6
 	
-		# need to kiv the different sensors and the number of bytes of data sent respectively
 			elif CurrMode == 6 :
 				print("crc")
-				if crcCount > -1:
-					print("~")
-					print(incomingByte)
-					crcData = crcData + pow(10.0,crcCount-1) * (incomingByte-48)
-					print(crcData)
-					crcCount = crcCount - 1
-					if crcCount == 0:
-						crcCount = 4
+				if crcSize > -1:
+					crcData[crcIndex] = incomingByte
+					crcIndex = crcIndex+1 
+					print(crcData[crcIndex])
+					crcSize = crcSize - 1
+					if crcSize == 0:
+						crcSize = 4
 						CurrMode = 7
-
 	
 			elif CurrMode == 7:
-				print("terminate")
+				print("Terminate")
 				if incomingByte != 62 :
 					CurrMode = 8
 					print("CORRUPT")
 					readStatus = False
 				else:
-					CurrMode = 0;
-					print(componentID[0])
-					print(payloadData)
-					print(crcData)
+					CurrMode = 0
+					print(data)
+					print(crcData[0])
+					print(crcData[1])
+					print(crcData[2])
+					print(crcData[3])
 					readStatus = False
+					#add switch case (if ack/nack/data received what happens)
+					#if packet_type == 1: 
+					#	ser.write(40)
+					#elif packet_type ==2:
+					#	ser.write()
+					#elif packet_type ==3:
+					#	ser.write()
 		
 			elif CurrMode == 8:
-				print("handling corrupt packet")
+				print("Handling Corrupt Packet")
 				CurrMode = 0
 				readStatus = False
 
@@ -162,15 +148,11 @@ ser.write("<")
 time.sleep(1)
 ser.write(",")
 time.sleep(1)
-ser.write("005")
+ser.write(1)
 time.sleep(1)
-ser.write("1")
+ser.write(2)
 time.sleep(1)
-ser.write("01")
-time.sleep(1)
-ser.write("@")
-time.sleep(1)
-ser.write("22222")
+ser.write(232)
 time.sleep(1)
 ser.write("1111")
 time.sleep(1)
