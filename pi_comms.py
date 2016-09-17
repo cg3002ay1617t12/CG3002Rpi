e767679c5c6ee9ff3220e5607cb1605cccaf0c04
Joanna <joannakxt@ymail.com>
2016-09-17 17:48:43 +0800

    removed while loop

--- pi_comms.py
+++ pi_comms.py
@@ -56,94 +56,94 @@ def read():
 	if ser.inWaiting()>0:
 		print "wait > 0"
 		readStatus = True
-		while readStatus:
-			print("enter read mode")
-			incomingByte = ser.read()
-			print(incomingByte)
-			if CurrMode == 0: 
-				if incomingByte == 60:
-					CurrMode = 1
-					print("Recieved Start")
+		#while readStatus:
+		print("enter read mode")
+		incomingByte = ser.read()
+		print(incomingByte)
+		if CurrMode == 0: 
+			if incomingByte == 60:
+				CurrMode = 1
+				print("Recieved Start")
 
-			elif CurrMode == 1:
-				if incomingByte == 40:
-					print("Recieved ACK")
-					packet_type = 1 
-					CurrMode = 3
-				elif incomingByte == 41:
-					print("Recieved NACK")
-					packet_type = 2
-					CurrMode = 3
-				elif incomingByte == 42:
-					print("Recieved Prob")
-					packet_type = 3
-					CurrMode = 3
-				elif incomingByte == 43:
-					print("Recieved Read")
-					packet_type = 4
-					CurrMode = 3
-				elif incomingByte == 44:
-					print("Recieved Data")
-					packet_type = 5
-					CurrMode = 3
-				elif incomingByte == 45:
-					print("Recieved Request")
-					packet_type = 6
-					CurrMode = 3
-				else:
-					CurrMode = 8
-					print("CORRUPT")
+		elif CurrMode == 1:
+			if incomingByte == 40:
+				print("Recieved ACK")
+				packet_type = 1 
+				CurrMode = 3
+			elif incomingByte == 41:
+				print("Recieved NACK")
+				packet_type = 2
+				CurrMode = 3
+			elif incomingByte == 42:
+				print("Recieved Prob")
+				packet_type = 3
+				CurrMode = 3
+			elif incomingByte == 43:
+				print("Recieved Read")
+				packet_type = 4
+				CurrMode = 3
+			elif incomingByte == 44:
+				print("Recieved Data")
+				packet_type = 5
+				CurrMode = 3
+			elif incomingByte == 45:
+				print("Recieved Request")
+				packet_type = 6
+				CurrMode = 3
+			else:
+				CurrMode = 8
+				print("CORRUPT")
 			
-			elif CurrMode == 3 :
-				print("Payload_seq")
-				if packet_seq == 0: 
-					CurrMode = 8
-					print("CORRUPT")
-				else: 
-					CurrMode = 4
-					packet_seq = not packet_seq
+		elif CurrMode == 3 :
+			print("Payload_seq")
+			if packet_seq == 0: 
+				CurrMode = 8
+				print("CORRUPT")
+			else: 
+				CurrMode = 4
+				packet_seq = not packet_seq
 		
-			elif CurrMode == 4 :
-				print("component_id")
-				if incomingByte >0 and incomingByte <41: 
-					CurrMode = 5 
-				else:
-					CurrMode = 8
-					print("CORRUPT")
+		elif CurrMode == 4 :
+			print("component_id")
+			if incomingByte >0 and incomingByte <41: 
+				CurrMode = 5 
+			else:
+				CurrMode = 8
+				print("CORRUPT")
 		
-			elif CurrMode == 5 :
-				print("payload")
-				if dataIndex > -1: 
-					data = data + incomingByte
-					dataIndex = dataIndex-1 
-					if dataIndex == 0:
-						CurrMode = 6
+		elif CurrMode == 5 :
+			print("payload")
+			if dataIndex > -1: 
+				data = data + incomingByte
+				dataIndex = dataIndex-1 
+				if dataIndex == 0:
+					CurrMode = 6
 	
-			elif CurrMode == 6 :
-				print("crc")
-				if crcSize > -1:
-					crcData[crcIndex] = incomingByte
-					crcIndex = crcIndex+1 
-					print(crcData[crcIndex])
-					crcSize = crcSize - 1
-					if crcSize == 0:
-						crcSize = 4
-						CurrMode = 7
+		elif CurrMode == 6 :
+			print("crc")
+			if crcSize > -1:
+				crcData[crcIndex] = incomingByte
+				crcIndex = crcIndex+1 
+				print(crcData[crcIndex])
+				crcSize = crcSize - 1
+				if crcSize == 0:
+					crcSize = 4
+					CurrMode = 7
 	
-			elif CurrMode == 7:
-				print("Terminate")
-				if incomingByte != 62 :
-					CurrMode = 8
-					print("CORRUPT")
-					readStatus = False
-				else:
-					CurrMode = 0
-					print(data)
-					print(crcData[0])
-					print(crcData[1])
-					print(crcData[2])
-					print(crcData[3])
-					readStatus = False
+		elif CurrMode == 7:
+			print("Terminate")
+			if incomingByte != 62 :
+				CurrMode = 8
+				print("CORRUPT")
+				readStatus = False
+			else:
+				CurrMode = 0
+				print(data)
+				print(crcData[0])
+				print(crcData[1])
+				print(crcData[2])
+				print(crcData[3])
+				readStatus = False
 					#add switch case (if ack/nack/data received what happens)
 					#if packet_type == 1: 
 					#	ser.write(40)
@@ -152,10 +152,10 @@ def read():
 					#elif packet_type ==3:
 					#	ser.write()
 		
-			elif CurrMode == 8:
-				print("Handling Corrupt Packet")
-				CurrMode = 0
-				readStatus = False
+		elif CurrMode == 8:
+			print("Handling Corrupt Packet")
+			CurrMode = 0
+			readStatus = False
 
 
 while 1:
