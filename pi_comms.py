95c3188144283e89b8ba6f03bd4292a31da3a4d6
Joanna <joannakxt@ymail.com>
2016-09-24 11:26:18 +0800

    simplified data handling

--- pi_comms.py
+++ pi_comms.py
@@ -4,47 +4,32 @@ import time
 import heapq
 import binascii
 
-#enum {0, 1, 2, 3, 4 , 5, 6, 7, 8 }
-
+#RECEIVE VARIABLES
 CurrMode = 0
-packet_type = 0 #save the type of packet
-
-booted = '0'
-bufferSize = 0
+packet_type = 0 #For packet handling
+packet_seq_RX = 0 
+data = 0 
+component_ID = 0
 
 ser = serial.Serial(port ='/dev/ttyAMA0', baudrate = 115200, timeout = 3)
 
-incomingByte = 0
-	
-packet_size = 0
-packet_size_count = 0
-	
-packet_seq = '0'
-
-dataSize = [] 
-data = 0 
-numComponent = 0 # MAX 10
-component_ID = 0
-componentFlag = '0'
-componentTemp = 0
-
-dataIndex = 8 
-payloadData = 1 # need to findout why is it it bugging out
-
-crcSize = 4
-crcIndex=0
-crcData = 0 # need to findout why is it it bugging out
-crcPoly = "100000111"
-txCrcIndex = 12
+incomingByte = 0 
+dataIndex = 8 #For payload
+crcData = 0 
 rxCrcIndex = 14
-	
 readStatus = False
-flag = 0
 
-packet_type_TS = 44 #PKT_TYPE IS DATA TO SEND
-packet_seq_TS = 0
-component_ID_TS = 12
-data_TS = 277
+#SEND VARIABLES
+createQueue = 1 
+txCrcIndex = 12
+protected_flag = 0 #1 means protect it, 0 means can override
+packet_type_TX = 44 #PKT_TYPE IS DATA TO SEND
+packet_seq_TX = 0 
+component_ID_TX = 12
+data_TX = 277
+
+#SHARED VARIABLES
+crcPoly = "100000111"
 
 class PriorityQueue:
 	def __init__(self):
@@ -69,18 +54,14 @@ def convert(number):
 		else: 
 			toSend.append(struct.pack('!B', number))
 			number = 0
-	divided_string = ''.join(toSend)
-	return divided_string
+	data_string = ''.join(toSend)
+	return data_string
 	
-
 def read(): 
 	global crcData
 	global CurrMode
 	if ser.inWaiting()>0:
-		#print "wait > 0"
 		readStatus = True
-		#while readStatus:
-		#print("enter read mode")
 		incomingByte = ser.read() 
 		if CurrMode == 0: 
 			incomingByte = ord(incomingByte)
@@ -91,27 +72,27 @@ def read():
 		elif CurrMode == 1:
 			incomingByte = ord(incomingByte)
 			if incomingByte == 40:
-				print("Recieved ACK")
+				print("Recieved HELLO")
 				packet_type = 1 
-				CurrMode = 3
+				CurrMode = 7
 			elif incomingByte == 41:
-				print("Recieved NACK")
+				print("Recieved ACK")
 				packet_type = 2
 				CurrMode = 3
 			elif incomingByte == 42:
-				print("Recieved Prob")
+				print("Recieved NACK")
 				packet_type = 3
 				CurrMode = 3
 			elif incomingByte == 43:
-				print("Recieved Read")
+				print("Recieved PROBE")
 				packet_type = 4
 				CurrMode = 3
 			elif incomingByte == 44:
-				print("Recieved Data")
+				print("Recieved REQ")
 				packet_type = 5
 				CurrMode = 3
 			elif incomingByte == 45:
-				print("Recieved Request")
+				print("Recieved DATA")
 				packet_type = 6
 				CurrMode = 3
 			else:
@@ -120,14 +101,14 @@ def read():
 			
 		elif CurrMode == 3 :
 			incomingByte = ord(incomingByte)
-			global packet_seq
+			global packet_seq_RX
 			print("Payload_seq")
-			if packet_seq == 0: 
+			if packet_seq_RX == 0: 
 				CurrMode = 8
 				print("CORRUPT")
 			else: 
 				CurrMode = 4
-				packet_seq = not packet_seq
+				packet_seq_RX = not packet_seq_RX
 		
 		elif CurrMode == 4 :
 			incomingByte = ord(incomingByte)
@@ -153,10 +134,6 @@ def read():
 	
 		elif CurrMode == 6 :
 			incomingByte = ord(incomingByte)
-			#incomingByte = int(incomingByte)
-			global crcSize
-			global crcIndex
-			#global crcData
 			print("crc")
 			if incomingByte == 1: 
 				CurrMode = 7 
@@ -174,35 +151,85 @@ def read():
 				print("CORRUPT")
 				readStatus = False
 			else:
+				print("Successfully")
 				CurrMode = 0
 				print(data)
 				print(crcData)
 				readStatus = False
-				#handling packets funct here
-				send();	
+				if packet_type ==1 or packet_type ==6:
+					handling_packets()
 		elif CurrMode == 8:
 			print("Handling Corrupt Packet")
 			CurrMode = 0
 			readStatus = False
 
-def send(): 
+def send(packet_type_S, component_ID_S, data_S): 
+	global packet_seq_TX
+	global packet_type_TX
+	global component_ID_TX
+	global data_TX
+
+	#Storing previous packet so it will not be overwritten
+	packet_type_TX = packet_type_S
+	component_ID_TX = component_ID_S
+	data_TX = data_S
+
 	ser.write("<")
-	ser.write(chr(packet_type_TS))
-	ser.write(chr(packet_seq_TS))
-	ser.write(chr(component_ID_TS))
-	ser.write(convert(data_TS))
+	ser.write(chr(packet_type_S))
+	ser.write(chr(packet_seq_TX))
+	ser.write(chr(component_ID_S))
+	ser.write(convert(data_S))
 	ser.write("1")
 	ser.write(">")
+	#protected_flag = 1 
 
 def handling_packets(): 
 	global packet_type
-	if packet_type == 3: #PROBE
-		send()
+	global packet_seq_TX
+	global packet_type_TX
+	global component_ID_TX
+	global data_TX
 
+	if packet_type == 1: #(HELLO RECEIVED) send hello back
+		#if protected_flag = 0: 
+		ser.write("<")
+		ser.write("(") 
+		ser.write(">")
+		#protected_flag = 1 
+		#else: 
+		#	queueData = {'p_type': 40, 'com_id' : -1 , 'p_data' : -1} 
+		#	q.push(queueData, 1)
+
+	#elif packet_type == 2: #(ACK RECEIVED) send ack with pkt_seq, set protected flag down, inverse pkt_seq and send next)
+	#	packet_seq_TX = not packet_seq_TX
+	#	protected_flag = 0
+	#	send(")", packet_seq_TX, component_ID_TX, data_TX) #Send ACK
+		#Send next by lowering protected_flag so send can be successful  
+
+	#elif packet_type == 3: #(NACK RECEIVED) resent packet
+	#	if protected_flag == 0: 
+	#		send(packet_type_TX, component_ID_TX, data_TX)
+	#	else: 
+	#		queueData = {'p_type': packet_type_TX, 'com_id' : component_ID_TX, 'p_data' : data_TX} 
+	#		q.push(queueData, 1)
+
+	#elif packet_type == 4: #(PROBE RECEIVED) send back ack followed by pkt_seq
+	#	if protected_flag == 0:
+	#		send(")", component_ID_TX, data_TX)
+	#	else:
+	#		queueData = {'p_type': 41, 'com_id' : component_ID_TX , 'p_data' : data_TX} 
+	#		q.push(queueData, 1)
+
+	elif packet_type == 6: #(DATA RECEIVED) send ack back with next pkt_seq
+		#if protected_flag == 0:
+		send(")", component_ID_TX, data_TX)
+		#else:
+		#	queueData = {'p_type': 41, 'com_id' : component_ID_TX , 'p_data' : data_TX}
+		#	q.push(queueData, 1) 
 
 def txCRC():
 	global packet_type
-	global packet_seq
+	global packet_seq_TX
 	global component_id
 	global data
 	toBeDivided = []
@@ -289,18 +316,20 @@ def encodeData(data, key):
 	print("Encoded Data (Data + Remainder Appended):")
 	print(codeword)
 
-#q = PriorityQueue() 
-#q.push("hello world", 5)
-#q.push("this is high priority", 3)
-#q.push("second highest", 4)
-#q.pop()
-#q.pop()
-#q.pop()
-
 while 1:
-	#read()
-	#txCRC()
-	#if flag==0:
-	#	encodeData(txCRC() ,crcPoly)
-	#	flag =1
+	#if createQueue: 
+	#	q = PriorityQueue() 
+	#	createQueue = 0 
+	#How to create this queue outside? 
+
 	read()
+
+	#if protected_flag == 0: 
+	#	toSend = q.pop() 
+	#	if toSend.get('p_type') == 40:   #Special case for re-sending HELLO packet 
+	#		ser.write("<")
+	#		ser.write("(") 
+	#		ser.write(">")
+	#		protected_flag = 1 
+	#	else: 
+	#		send(toSend.get('p_type'), toSend.get('com_id'), toSend.get('p_data'))
