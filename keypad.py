a1bd3220039ca9e4a0047d559d75d42d471167c5
thejerrytan <jerrytansk@gmail.com>
2016-10-17 22:10:38 +0800

    GPIO polling for keypad

--- keypad.py
+++ keypad.py
@@ -60,7 +60,7 @@ class KEY(object):
 # 	]
 ROW = [18,23,24,25] # G, H, J, K
 COL = [4,17,22] # D, E, F
-jf ->kf
+
 class Action(Enum):
 	APPEND        = 1
 	CLEAR         = 2
@@ -166,16 +166,10 @@ def action_on_transit(val, action):
 	else:
 		raise Exception("Unrecognized action!")
 
-def handler(row_id, val):
+def handler(key):
 	# Construct key object
-	print("Row : %d" % row_id)
+	print("Key triggered : %s" % str(key.value))
 	global state
-	for j in COL:
-		if GPIO.input(j) == 0:
-			col_id = j
-			print("Pin triggered: %d" % j)
-
-	key = KEY((row_id, col_id))
 	for transition in key.types:
 		try:
 			(state, action) = State.transitions[state][transition]
@@ -190,126 +184,133 @@ def setup():
 
 	for i in ROW:
 		GPIO.setup(i, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-		RPIO.add_interrupt_callback(i, handler, edge="rising", threaded_callback=True, debounce_timeout_ms=100)
 
 def main():
 	setup()
-	# os.system('flite -t "Enter, start, destination." ')
-	RPIO.wait_for_interrupts(threaded=True)
+	try:
+		while(True):
+			for j in COL:
+				GPIO.output(j, 0)
+				for i in ROW:
+					while (GPIO.input(i) == 0):
+						pass
+					time.sleep(0.1)
+					Key = KEY((i, j))
+				GPIO.output(j, 1)
+	except KeyboardInterrupt as e:
+		GPIO.cleanup()
 
-# try:
-# 	while(True):
-# 		for j in range(3):
-# 			GPIO.output(COL[j], 0)
+	# try:
+	# 	while(True):
+	# 		for j in COL:
+	# 			GPIO.output(j, 0)
+	# 			for i in row:
+	# 				if GPIO.input(i) == 0:
+	# 					if i == 0:
+	# 						if j == 0:
+	# 							while(GPIO.input(ROW[i]) == 0):
+	# 								pass
+	# 							time.sleep(0.1)
+	# 							number = 1
+	# 							node += str(number)
+	# 						if j == 1:
+	# 							while(GPIO.input(ROW[i]) == 0):
+	# 								pass
+	# 							time.sleep(0.1)
+	# 							number = 2
+	# 							node += str(number)
+	# 						if j == 2:
+	# 							while(GPIO.input(ROW[i]) == 0):
+	# 								pass
+	# 							time.sleep(0.1)
+	# 							number = 3
+	# 							node += str(number)
+	# 					elif i == 1:
+	# 						if j == 0:
+	# 							while(GPIO.input(ROW[i]) == 0):
+	# 								pass
+	# 							time.sleep(0.1)
+	# 							number = 4
+	# 							node += str(number)
+	# 						if j == 1:
+	# 							while(GPIO.input(ROW[i]) == 0):
+	# 								pass
+	# 							time.sleep(0.1)
+	# 							number = 5
+	# 							node += str(number)
+	# 						if j == 2:
+	# 							while(GPIO.input(ROW[i]) == 0):
+	# 								pass
+	# 							time.sleep(0.1)
+	# 							number = 6
+	# 							node += str(number)
+	# 					elif i == 2:
+	# 						if j == 0:
+	# 							while(GPIO.input(ROW[i]) == 0):
+	# 								pass
+	# 							time.sleep(0.1)
+	# 							number = 7
+	# 							node += str(number)
+	# 						if j == 1:
+	# 							while(GPIO.input(ROW[i]) == 0):
+	# 								pass
+	# 							time.sleep(0.1)
+	# 							number = 8
+	# 							node += str(number)
+	# 						if j == 2:
+	# 							while(GPIO.input(ROW[i]) == 0):
+	# 								pass
+	# 							time.sleep(0.1)
+	# 							number = 9
+	# 							node += str(number)
+	# 					elif i == 3:
+	# 						if j == 1:
+	# 							while(GPIO.input(ROW[i]) == 0):
+	# 								pass
+	# 							time.sleep(0.1)
+	# 							number = 0
+	# 							node += str(number)
+	# 						if j == 0:
+	# 							if len(node) == 2:
+	# 								while(GPIO.input(ROW[i]) == 0):
+	# 									pass
+	# 								time.sleep(0.1)
+	# 								print node
+	# 								if state == 0:
+	# 									start=node
+	# 									print start
+	# 									os.system("flite -t 'Start, destination, is, "+node+" ' ")
+	# 									node=''
+	# 									state=1
+	# 									print state
+	# 									os.system('flite -t "Enter, end, destination." ')
+	# 								if state == 1:
+	# 									if len(node) == 2:
+	# 										end = node
+	# 										print end
+	# 										os.system("flite -t 'End, destination, is, "+node+" ' ")
+	# 										node=''
+	# 										os.system('flite -t "Entering, navigation, mode." ')
+	# 						if j == 2:
+	# 							while(GPIO.input(ROW[i]) == 0):
+	# 								pass
+	# 							time.sleep(0.1)
+	# 							node=''
+	# 							os.system('flite -t "Re-enter, destination." ')
+	# 					if len(node) == 2:
+	# 						print node
+	# 						#os.system("flite -voice slt -t 'Your, start, destination, is, "+node+" ' ")
+	# 						if state == 0:
+	# 							os.system("flite -t 'Start, destination, is, "+node+" ' ")
+	# 						if state == 1:
+	# 							os.system("flite -t 'End, destination, is, "+node+" ' ")
+	# 						os.system('flite -t "To, confirm, press, *." ')
+	# 						os.system('flite -t "To, re-enter, destination, press, #." ')
+	# 						time.sleep(0.25)
 
-# 			for i in range(4):
-# 				if GPIO.input(ROW[i]) == 0:
-# 					if i == 0:
-# 						if j == 0:
-# 							while(GPIO.input(ROW[i]) == 0):
-# 								pass
-# 							time.sleep(0.1)
-# 							number = 1
-# 							node += str(number)
-# 						if j == 1:
-# 							while(GPIO.input(ROW[i]) == 0):
-# 								pass
-# 							time.sleep(0.1)
-# 							number = 2
-# 							node += str(number)
-# 						if j == 2:
-# 							while(GPIO.input(ROW[i]) == 0):
-# 								pass
-# 							time.sleep(0.1)
-# 							number = 3
-# 							node += str(number)
-# 					elif i == 1:
-# 						if j == 0:
-# 							while(GPIO.input(ROW[i]) == 0):
-# 								pass
-# 							time.sleep(0.1)
-# 							number = 4
-# 							node += str(number)
-# 						if j == 1:
-# 							while(GPIO.input(ROW[i]) == 0):
-# 								pass
-# 							time.sleep(0.1)
-# 							number = 5
-# 							node += str(number)
-# 						if j == 2:
-# 							while(GPIO.input(ROW[i]) == 0):
-# 								pass
-# 							time.sleep(0.1)
-# 							number = 6
-# 							node += str(number)
-# 					elif i == 2:
-# 						if j == 0:
-# 							while(GPIO.input(ROW[i]) == 0):
-# 								pass
-# 							time.sleep(0.1)
-# 							number = 7
-# 							node += str(number)
-# 						if j == 1:
-# 							while(GPIO.input(ROW[i]) == 0):
-# 								pass
-# 							time.sleep(0.1)
-# 							number = 8
-# 							node += str(number)
-# 						if j == 2:
-# 							while(GPIO.input(ROW[i]) == 0):
-# 								pass
-# 							time.sleep(0.1)
-# 							number = 9
-# 							node += str(number)
-# 					elif i == 3:
-# 						if j == 1:
-# 							while(GPIO.input(ROW[i]) == 0):
-# 								pass
-# 							time.sleep(0.1)
-# 							number = 0
-# 							node += str(number)
-# 						if j == 0:
-# 							if len(node) == 2:
-# 								while(GPIO.input(ROW[i]) == 0):
-# 									pass
-# 								time.sleep(0.1)
-# 								print node
-# 								if state == 0:
-# 									start=node
-# 									print start
-# 									os.system("flite -t 'Start, destination, is, "+node+" ' ")
-# 									node=''
-# 									state=1
-# 									print state
-# 									os.system('flite -t "Enter, end, destination." ')
-# 								if state == 1:
-# 									if len(node) == 2:
-# 										end = node
-# 										print end
-# 										os.system("flite -t 'End, destination, is, "+node+" ' ")
-# 										node=''
-# 										os.system('flite -t "Entering, navigation, mode." ')
-# 						if j == 2:
-# 							while(GPIO.input(ROW[i]) == 0):
-# 								pass
-# 							time.sleep(0.1)
-# 							node=''
-# 							os.system('flite -t "Re-enter, destination." ')
-# 					if len(node) == 2:
-# 						print node
-# 						#os.system("flite -voice slt -t 'Your, start, destination, is, "+node+" ' ")
-# 						if state == 0:
-# 							os.system("flite -t 'Start, destination, is, "+node+" ' ")
-# 						if state == 1:
-# 							os.system("flite -t 'End, destination, is, "+node+" ' ")
-# 						os.system('flite -t "To, confirm, press, *." ')
-# 						os.system('flite -t "To, re-enter, destination, press, #." ')
-# 						time.sleep(0.25)
-
-# 			GPIO.output(COL[j], 1)
-
-# except KeyboardInterrupt:
-# 	GPIO.cleanup()
+	# 			GPIO.output(COL[j], 1)
+	# except KeyboardInterrupt:
+	# 	GPIO.cleanup()
 
 if __name__ == "__main__":
 	main()
