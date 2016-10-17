8925b8a2f46b2b07b27aac86f31f4deed0449fb0
thejerrytan <jerrytansk@gmail.com>
2016-10-18 03:45:49 +0800

    print q len on run

--- audio.py
+++ audio.py
@@ -12,11 +12,13 @@ class AudioQueue(object):
 			print(instruction % placeholders)
 		args = shlex.split('flite -t ' + ("\"" + instruction % placeholders + "\""))
 		self.q.append(args)
+		print("tts : "),
 		print(len(self.q))
 
 	def run(self):
-		print("Started playing audio queue!")
 		while True:
+			print("run : "),
+			print(len(self.q))
 			if len(self.q) > 0:
 				process = subprocess.Popen(self.q.popleft())
 				print("Process opened")
