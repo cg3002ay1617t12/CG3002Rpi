d3af1e392c31fb7448bd3f9aaca0c95f882a4d70
thejerrytan <jerrytansk@gmail.com>
2016-10-18 00:04:09 +0800

    added [INSTRUCTION] at start of tts print

--- audio.py
+++ audio.py
@@ -2,5 +2,7 @@ import os
 
 def tts(instruction, placeholders=(), verbose=True):
 	if verbose:
+		print("[INSTRUCTION] : "),
 		print(instruction % placeholders)
+	print instruction
 	os.system('flite -t ' + ("\"" + instruction + "\"" % placeholders))
