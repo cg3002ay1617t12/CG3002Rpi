909dfa37cf123e6bcc8663e79dd3f21860bfbbb9
Seokhoon Lee <seokhoon.lee.9393@gmail.com>
2016-10-19 00:02:08 +0800

    Fix angle and add test more extensive test cases

--- path_finder.py
+++ path_finder.py
@@ -2,80 +2,148 @@ import json, requests, math, heapq, pprint
 
 class PathFinder(object):
 	def __init__(self):
-		self.request_url = 'http://showmyway.comp.nus.edu.sg/getMapInfo.php?Building=Com1&Level=2'
-		# self.request_url = 'http://showmyway.comp.nus.edu.sg/getMapInfo.php?Building=DemoBuilding&Level=1'
-		self.wifi_radius = 150
-		self.reach_radius = 50
+		self.__request_url = 'http://showmyway.comp.nus.edu.sg/getMapInfo.php?Building=Com1&Level=2'
+		# self.__request_url = 'http://showmyway.comp.nus.edu.sg/getMapInfo.php?Building=DemoBuilding&Level=1'
+		self.__wifi_radius = 150
+		self.__reach_radius = 50
 
-		self.node_info = None
-		self.adjacency_matrix = None
-		self.num_node = -1
-		self.angle_of_north = -1
+		self.__node_info = None
+		self.__adjacency_matrix = None
+		self.__num_node = -1
+		self.__angle_of_north = -1
 
-		self.x_coordinate = -1
-		self.y_coordinate = -1
-		self.angle = -1
+		self.__x_coordinate = -1
+		self.__y_coordinate = -1
+		self.__angle = -1
 
-		self.source = -1
-		self.target = -1
+		self.__source = -1
+		self.__target = -1
 
-		self.shortest_path = []
-		self.visited_nodes = []
-		self.next_node = -1
+		self.__shortest_path = []
+		self.__visited_nodes = []
+		self.__next_node = -1
 
-		self.instruction = []
+		self.__instruction = []
 
-		self.update_node_info()
+		self.__update_node_info()
+
+	""" PUBLIC FUNCTION """
+	# def is_initialized(self):
+	# 	return (self.__node_info != None and self.__adjacency_matrix != None and self.__angle_of_north != -1 and self.__num_node != -1)
 
 	def is_ready(self):
-		return (self.node_info != None and self.adjacency_matrix != None and self.x_coordinate != -1 and self.y_coordinate != -1)
+		return (self.__x_coordinate != -1 and self.__y_coordinate != -1 and self.__angle != -1)
 
 	def update_coordinate(self, x_coordinate, y_coordinate, angle_from_north):
-		self.x_coordinate = x_coordinate
-		self.y_coordinate = y_coordinate
-		# print(x_coordinate, y_coordinate)
-		self.angle = self.get_angle_wrt_north(angle_from_north)
+		self.__x_coordinate = x_coordinate
+		self.__y_coordinate = y_coordinate
+		self.__angle = self.__get_angle_wrt_grid(angle_from_north)
+		# self.__angle = angle_from_north
 
 		node_reached = -1
 		reached = False
 
-		if self.next_node != -1:
-			if self.is_reached(self.next_node, x_coordinate, y_coordinate):
-				node_reached = self.next_node
+		if self.__next_node != -1:
+			if self.__is_reached(self.__next_node, self.__x_coordinate, self.__y_coordinate):
+				node_reached = self.__next_node
 				reached = True
-				self.update_visited_nodes(self.next_node)
+				self.__update_visited_nodes(self.__next_node)
 			else:
-				self.update_instruction()
+				self.__update_instruction()
 
 		return reached, node_reached
 
-	def coordinates_from_node(self, node):
-		node_info = self.node_info[node]
+	def update_source_and_target(self, source, target):
+		if not self.is_ready():
+			return False
+
+		if source < 1 or target < 1 or source > self.__num_node or target > self.__num_node:
+			return False
+
+		self.__source = source
+		self.__target = target
+
+		self.__update_shortest_path()
+
+	def get_audio_next_instruction(self):
+		if self.__node_info == None:
+			return 'Not Ready'
+
+		if not self.__instruction:
+			return 'No Instruction'
+
+		instruction = self.__instruction[0]
+
+		from_index = instruction['from_index']
+		to_index = instruction['to_index']
+
+		if from_index == 0:
+			from_name = 'Current Position'
+		else:
+			from_name = self.__node_info[from_index]['name']
+
+		to_name = self.__node_info[to_index]['name']
+
+		distance = instruction['distance']
+		
+		angle = instruction['angle']
+
+		right = True
+
+		if angle > 180:
+			right = False
+			angle -= 180
+
+		audio_string = 'From Node ' + str(from_index) + ' ' + str(from_name) + ' To Node ' + str(to_index) + ' ' + str(to_name) + ':'
+
+		if right:
+			audio_string += 'Turn Right ' + str(angle) + ' And Go ' + str(distance)
+		else:
+			audio_string += 'Turn Left ' + str(angle) + ' And Go ' + str(distance)
+
+		return audio_string
+
+	def get_audio_reached(self, reached_index):
+		if self.__node_info == None:
+			return 'Not Ready'
+
+		reached_name = self.__node_info[reached_index]['name']
+
+		audio_string = 'Reached Node ' +  str(reached_index) + ' ' + str(reached_name)
+
+		return audio_string
+
+	def get_angle_of_north(self):
+		return self.__angle_of_north
+
+	def get_x_coordinate(self):
+		return self.__x_coordinate
+
+	def get_y_coordinate(self):
+		return self.__y_coordinate
+
+	""" PRIVATE FUNCTION """
+	def __coordinates_from_node(self, node):
+		node_info = self.__node_info[node]
 		return (node_info['x'], node_info['y'])
 
-	def is_reached(self, node_index, user_x, user_y):
-		node_x = self.node_info[node_index]['x']
-		node_y = self.node_info[node_index]['y']
+	def __is_reached(self, node_index, user_x, user_y):
+		node_x = self.__node_info[node_index]['x']
+		node_y = self.__node_info[node_index]['y']
 
-		if self.get_distance(user_x, user_y, node_x, node_y) < self.reach_radius:
+		if self.__get_distance(user_x, user_y, node_x, node_y) < self.__reach_radius:
 			return True
 		else:
 			return False
 
-	def update_source_and_target(self, source, target):
-		self.source = source
-		self.target = target
+	def __update_visited_nodes(self, node):
+		if node not in self.__visited_nodes:
+			self.__visited_nodes.append(node)
 
-		self.update_shortest_path()
+		self.__update_instruction()
 
-	def update_visited_nodes(self, node):
-		if node not in self.visited_nodes:
-			self.visited_nodes.append(node)
-
-		self.update_instruction()
-
-	def update_node_info(self):
-		request_info = requests.get(self.request_url)
+	def __update_node_info(self):
+		request_info = requests.get(self.__request_url)
 
 		json_request_info = json.loads(request_info.text)
 
@@ -89,10 +157,10 @@ class PathFinder(object):
 		#     'y': int(node['y'])
 		#   }
 
-		self.node_info = {}
+		self.__node_info = {}
 
 		for node in json_request_info['map']:
-			self.node_info[int(node['nodeId'])] = {
+			self.__node_info[int(node['nodeId'])] = {
 			'name': node['nodeName'],
 			'neighbour' : [int(node_index.strip()) for node_index in str(node['linkTo']).split(',')],
 			'x': int(node['x']),
@@ -100,7 +168,7 @@ class PathFinder(object):
 			'wifi': []
 			}
 
-		# for node_id, node in self.node_info.items():
+		# for node_id, node in self.__node_info.items():
 		#   node_x = node['x']
 		#   node_y = node['y']
 			
@@ -113,46 +181,43 @@ class PathFinder(object):
 
 		#     diff = int(math.sqrt(delta_x * delta_x + delta_y * delta_y))
 			
-		#     if diff < self.wifi_radius:
-		#       self.node_info[node_id]['wifi'].append((wifi['name'], wifi['mac']))
+		#     if diff < self.__wifi_radius:
+		#       self.__node_info[node_id]['wifi'].append((wifi['name'], wifi['mac']))
 
-		self.angle_of_north = int(json_request_info['info']['northAt'])
+		self.__angle_of_north = int(json_request_info['info']['northAt'])
 
-		self.update_adjacency_matrix()
+		self.__update_adjacency_matrix()
 
-	def update_adjacency_matrix(self):
-		self.num_node = len(self.node_info)
+	def __update_adjacency_matrix(self):
+		self.__num_node = len(self.__node_info)
 
-		self.adjacency_matrix = [[-1 for row in range(self.num_node + 1)] for col in range(self.num_node + 1)]
+		self.__adjacency_matrix = [[-1 for row in range(self.__num_node + 1)] for col in range(self.__num_node + 1)]
 
-		for index, node in self.node_info.iteritems():
+		for index, node in self.__node_info.iteritems():
 			current_index = index
 			current_x = node['x']
 			current_y = node['y']
 
 			for neighbour_index in node['neighbour']:
-				neighbour_x = self.node_info[neighbour_index]['x']
-				neighbour_y = self.node_info[neighbour_index]['y']
+				neighbour_x = self.__node_info[neighbour_index]['x']
+				neighbour_y = self.__node_info[neighbour_index]['y']
 
 				delta_x = current_x - neighbour_x 
 				delta_y = current_y - neighbour_y
 
 				weight = int(math.sqrt(delta_x * delta_x + delta_y * delta_y))
 
-				self.adjacency_matrix[current_index][neighbour_index] = weight
-				self.adjacency_matrix[neighbour_index][current_index] = weight
+				self.__adjacency_matrix[current_index][neighbour_index] = weight
+				self.__adjacency_matrix[neighbour_index][current_index] = weight
 
-	def update_shortest_path(self):
-		start_index = self.source
-		end_index = self.target
-
-		if start_index < 1 or end_index < 1 or start_index > self.num_node or end_index > self.num_node:
-			return [-1], [-1]
+	def __update_shortest_path(self):
+		start_index = self.__source
+		end_index = self.__target
 
 		priority_queue = []
-		visited = [False] * (self.num_node + 1)
-		# distance = [0 for i in range(self.num_node + 1)]
-		predecesor = [-1 for i in range(self.num_node + 1)]
+		visited = [False] * (self.__num_node + 1)
+		# distance = [0 for i in range(self.__num_node + 1)]
+		predecesor = [-1 for i in range(self.__num_node + 1)]
 
 		init_weight = 0
 		init_index = start_index
@@ -178,7 +243,7 @@ class PathFinder(object):
 			# if pop_index == end_index:
 			#   break
 
-			for neighbour_index, neighbour_weight in enumerate(self.adjacency_matrix[pop_index]):
+			for neighbour_index, neighbour_weight in enumerate(self.__adjacency_matrix[pop_index]):
 				if neighbour_weight != -1 and not visited[neighbour_index]:
 					push_weight = pop_weight + neighbour_weight
 					push_index = neighbour_index
@@ -188,51 +253,55 @@ class PathFinder(object):
 
 					heapq.heappush(priority_queue, push_tuple)
 
-		self.shortest_path = []
+		self.__shortest_path = []
 
 		while 1:
-			self.shortest_path.append(end_index)
+			self.__shortest_path.append(end_index)
 			if end_index == start_index:
 				break
 			end_index = predecesor[end_index]
 
-		self.shortest_path.reverse()
+		self.__shortest_path.reverse()
 
-		self.update_instruction()
+		self.__update_instruction()
 
 		# return shortest_path, distance
 
-	def update_instruction(self):
-		self.instruction = []
+	def __update_instruction(self):
+		self.__instruction = []
 
 		remaining_path = []
 
-		for index in self.shortest_path:
-			if index not in self.visited_nodes:
+		for index in self.__shortest_path:
+			if index not in self.__visited_nodes:
 				remaining_path.append(index)
 
-		self.next_node = remaining_path[0]
+		if not remaining_path:
+			self.__instruction =  []
+			return 
 
-		prev_x = self.x_coordinate
-		prev_y = self.y_coordinate
+		self.__next_node = remaining_path[0]
+
+		prev_x = self.__x_coordinate
+		prev_y = self.__y_coordinate
 		prev_index = 0
-		prev_angle = self.angle
+		prev_angle = self.__angle
 
 		for index in remaining_path:
-			curr_name = self.node_info[index]['name']
-			curr_x = self.node_info[index]['x']
-			curr_y = self.node_info[index]['y']
+			curr_name = self.__node_info[index]['name']
+			curr_x = self.__node_info[index]['x']
+			curr_y = self.__node_info[index]['y']
 			curr_index = index
 
-			distance = self.get_distance(prev_x, prev_y, curr_x, curr_y)
+			distance = self.__get_distance(prev_x, prev_y, curr_x, curr_y)
 
-			curr_angle = self.get_angle(prev_x, prev_y, curr_x, curr_y)
+			curr_angle = self.__get_angle(prev_x, prev_y, curr_x, curr_y)
 
-			self.instruction.append({
+			self.__instruction.append({
 				'from_index': prev_index,
 				'to_index': curr_index,
 				'distance': distance,
-				'angle': self.get_angle_change(prev_angle, curr_angle)
+				'angle': self.__get_angle_change(prev_angle, curr_angle)
 			})
 
 			prev_name = curr_name
@@ -241,130 +310,196 @@ class PathFinder(object):
 			prev_index = curr_index
 			prev_angle = curr_angle
 
-	def get_distance(self, x1, y1, x2, y2):
+	def __get_distance(self, x1, y1, x2, y2):
 		x_diff = x2 - x1
 		y_diff = y2 - y1
 
 		return int(math.sqrt(x_diff * x_diff + y_diff * y_diff))
 
-	def get_angle_wrt_north(self, angle):
-		angle = self.angle_of_north - angle
+	def __get_angle_wrt_grid(self, angle):
+		angle = self.__angle_of_north + angle
 
-		while angle < 0:
-			angle += 360
-
-		if angle > 180:
-			angle -= 360
+		angle = self.__convert_angle_to_convention(angle)
 
 		return angle
 
-	def get_angle(self, x1, y1, x2, y2):
+	def __get_angle(self, x1, y1, x2, y2):
 		x_diff = x2 - x1
 		y_diff = y2 - y1
 
 		angle = int(math.degrees(math.atan2(x_diff, y_diff)))
 
-		while angle < 0:
-			angle += 360
-
-		if angle > 180:
-			angle -= 360
+		angle = self.__convert_angle_to_convention(angle)
 
 		return angle
 
-	def get_angle_change(self, angle_1, angle_2):
+	def __get_angle_change(self, angle_1, angle_2):
 		angle = angle_2 - angle_1
 
+		angle = self.__convert_angle_to_convention(angle)
+
+		return angle
+
+	def __convert_angle_to_convention(self, angle):
 		while angle < 0:
 			angle += 360
 
-		if angle > 180:
+		while angle >= 360:
 			angle -= 360
 
 		return angle
 
-	def get_audio_next_instruction(self, instruction):
-		if not instruction:
-			return 'No Instruction'
-
-		instruction = instruction[0]
-
-		from_index = instruction['from_index']
-		to_index = instruction['to_index']
-
-		if from_index == 0:
-			from_name = 'Current Position'
-		else:
-			from_name = self.node_info[from_index]['name']
-
-		to_name = self.node_info[to_index]['name']
-
-		distance = instruction['distance']
-		
-		angle = instruction['angle']
-
-		right = True
-
-		if angle < 0:
-			right = False
-			angle *= -1
-
-		audio_string = 'From Node ' + str(from_index) + ' ' + str(from_name) + ' To Node ' + str(to_index) + ' ' + str(to_name) + ':'
-
-		if right:
-			audio_string += 'Turn Right ' + str(angle) + ' And Go ' + str(distance)
-		else:
-			audio_string += 'Turn Left ' + str(angle) + ' And Go ' + str(distance)
-
-		return audio_string
-
-	def get_audio_reached(self, reached_index):
-		reached_name = self.node_info[reached_index]['name']
-
-		audio_string = 'Reached Node ' +  str(reached_index) + ' ' + str(reached_name)
-
-		return audio_string
 
 if __name__ == "__main__":
-	x = 2352
-	y = 2636
-	a = 0
-	src = 12
-	dst = 24
-	vst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+	def test_visit():
+		print 'testing::test_visit(): started visit test'
 
-	pf = PathFinder()
+		pf = PathFinder()
 
-	if pf.is_ready():
-		pf.update_source_and_target(src, dst) # update_source_and_target should only be used if ready
+		source = 1
+		target = pf._PathFinder__num_node
 
-	reached, node_reached = pf.update_coordinate(x, y, a)
-	if reached:
-		print pf.get_audio_reached(node_reached)
+		pf.update_coordinate(0, 0, 0)
+		pf.update_source_and_target(source, target)
 
-	if pf.is_ready():
-		pf.update_source_and_target(src, dst) # update_source_and_target should only be used if ready
+		shortest_path = pf._PathFinder__shortest_path
 
-	print 'Before Update'
-	print pf.get_audio_next_instruction(pf.instruction)
-	# for i in pf.instruction:
-	#   print i
+		for i in range(source, target + 1):
+			reached, node_reached = pf.update_coordinate(pf._PathFinder__node_info[i]['x'], pf._PathFinder__node_info[i]['y'], 0)
+			if reached:
+				if node_reached not in shortest_path:
+					print 'Error'
+					print 'source' + str(source)
+					print 'target' + str(target)
+					print 'node reached: ' + str(node_reached)
+					print 'shortest path: ' + str(shortest_path)
 
-	for i in vst:
-		pf.update_visited_nodes(i) # update_visited_nodes is not meant to be called, called for testing purposes for now
+		print 'testing::test_visit(): completed visit test'
 
-	print 'After Update'
-	print pf.get_audio_next_instruction(pf.instruction)
-	# for i in pf.instruction:
-	#   print i
+	def test_angle():
+		print 'testing::test_angle(): started angle test'
 
-	print 'After Moving'
+		pf = PathFinder()
 
-	reached, node_reached = pf.update_coordinate(5603, 1750, 0)
+		source = 1
+		target = 1
 
-	if reached:
-		print pf.get_audio_reached(node_reached)
+		pf.update_coordinate(0, 0, 0)
+		pf.update_source_and_target(source, target)
 
-	print pf.get_audio_next_instruction(pf.instruction)
-	# for i in pf.instruction:
-	#   print i
+		sign_x = [0, 1, 1, 1, 0, -1, -1, -1]
+		sign_y = [1, 1, 0, -1, -1, -1, 0, 1]
+
+		for i in range(0, 8):
+			x_coordinate = pf._PathFinder__node_info[1]['x'] + (100 * sign_x[i])
+			y_coordinate = pf._PathFinder__node_info[1]['y'] + (100 * sign_y[i])
+			angle_of_user_from_north = 0
+			expected_angle = pf._PathFinder__convert_angle_to_convention((360 - pf._PathFinder__angle_of_north) + 180 + (i * 45))
+
+			for j in range(0, 8):
+				pf.update_coordinate(x_coordinate, y_coordinate, angle_of_user_from_north + (j * 45))
+				if pf._PathFinder__instruction[0]['angle'] != pf._PathFinder__convert_angle_to_convention(expected_angle - (j * 45)):
+					print 'Error'
+					print 'user x: ' + str(x_coordinate)
+					print 'user y: ' + str(y_coordinate)
+					print 'user angle from north: ' + str(pf._PathFinder__convert_angle_to_convention(angle_of_user_from_north))
+					print 'next x: ' + str(pf._PathFinder__node_info[1]['x'])
+					print 'next y: ' + str(pf._PathFinder__node_info[1]['y'])
+					print 'next angle: ' + str(pf._PathFinder__instruction[0]['angle'])
+					print 'expected angle: ' + str(pf._PathFinder__convert_angle_to_convention(expected_angle))
+
+		print 'testing::test_angle(): completed angle test'
+
+	def test_instruction():
+		print 'testing::test_instruction(): started instruction test'
+
+		pf = PathFinder()
+
+		source = 1
+		target = pf._PathFinder__num_node
+
+		pf.update_coordinate(0, 0, 0)
+		pf.update_source_and_target(source, target)
+
+		shortest_path = pf._PathFinder__shortest_path
+
+		len_instruction = len(pf._PathFinder__instruction)
+
+		for i in range(0, len(shortest_path)):
+			reached, node_reached = pf.update_coordinate(pf._PathFinder__node_info[shortest_path[i]]['x'], pf._PathFinder__node_info[shortest_path[i]]['y'], 0)
+			
+			len_instruction -= 1
+
+			if len_instruction != len(pf._PathFinder__instruction):
+				print 'Error'
+				print 'instruction: ' + str(pf._PathFinder__instruction)
+
+		if len_instruction != 0:
+			print 'Error'
+
+		print 'testing::test_instruction(): completed instruction test'
+		
+	test_visit()
+	test_angle()
+	test_instruction()
+
+	# pf = PathFinder()
+
+	# print pf.node_info
+
+	# x = 2352
+	# y = 2636
+	# a = 0
+	# src = 2
+	# dst = 24
+	# vst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+	# pf = PathFinder()
+
+	# if pf.is_ready():
+	# 	pf.update_source_and_target(src, dst) # update_source_and_target should only be used if ready
+
+	# reached, node_reached = pf.update_coordinate(x, y, a)
+	# if reached:
+	# 	print pf.get_audio_reached(node_reached)
+
+	# if pf.is_ready():
+	# 	pf.update_source_and_target(src, dst) # update_source_and_target should only be used if ready
+
+	# print 'Before Update'
+	# # print pf.x_coordinate
+	# # print pf.y_coordinate
+	# # print pf.angle
+	# print pf.get_audio_next_instruction()
+	# # print pf.node_info[pf.next_node]['x']
+	# # print pf.node_info[pf.next_node]['y']
+	# # for i in pf.instruction:
+	# #   print i
+
+	# for i in vst:
+	# 	pf._PathFinder__update_visited_nodes(i) # __update_visited_nodes is not meant to be called, called for testing purposes for now
+
+	# print 'After Update'
+	# # print pf.x_coordinate
+	# # print pf.y_coordinate
+	# # print pf.angle
+	# print pf.get_audio_next_instruction()
+	# # print pf.node_info[pf.next_node]['x']
+	# # print pf.node_info[pf.next_node]['y']
+	# # for i in pf.instruction:
+	# #   print i
+
+	# print 'After Moving'
+
+	# reached, node_reached = pf.update_coordinate(5603, 1750, 0)
+	# if reached:
+	# 	print pf.get_audio_reached(node_reached)
+
+	# # print pf.x_coordinate
+	# # print pf.y_coordinate
+	# # print pf.angle
+	# print pf.get_audio_next_instruction()
+	# # print pf.node_info[pf.next_node]['x']
+	# # print pf.node_info[pf.next_node]['y']
+	# # for i in pf.instruction:
+	# #   print i
