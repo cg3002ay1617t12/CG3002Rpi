21221c52b8971c8c6c5caf590bf51ed99d94719f
thejerrytan <jerrytansk@gmail.com>
2016-10-28 19:16:58 +0800

    commit

--- main.py
+++ main.py
@@ -242,7 +242,7 @@ class App(object):
 					# Do something, make sure its non-blocking
 					self.StepDetector.run()
 					angle = self.PathFinder.get_angle_to_next_node()
-					print '[MAIN] Angle: ' + str(angle)
+					# print '[MAIN] Angle: ' + str(angle)
 					self.Localization.run(self.StepDetector.curr_steps, angle=angle)
 					if self.StepDetector.curr_steps > 0:
 						self.build_instruction("Step")
