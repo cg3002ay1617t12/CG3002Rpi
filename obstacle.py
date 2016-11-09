6a968365b0b010d28367d64cd01f3c512a6222a4
thejerrytan <jerrytansk@gmail.com>
2016-11-09 15:43:50 +0800

    save to byte stream and read to opencv numpy format

--- obstacle.py
+++ obstacle.py
@@ -1,4 +1,5 @@
-import cv2
+import cv2, io
+import numpy as np
 from picamera import PiCamera
 
 class ObstacleDetector(object):
@@ -21,14 +22,16 @@ class ObstacleDetector(object):
 
 	def run(self):
 		# Take a picture
-		img = self.cam.capture('./img.jpg')
-
+		stream = io.BytesIO()
+		pi_img = self.cam.capture(stream, format='jpeg')
+		data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+		cv_img = cv2.imdecode(data, 1)
 		# Detect banana
-		banana = self.banana_cascade.detectMultiScale(img, 1.2, 5)
+		banana = self.banana_cascade.detectMultiScale(cv_img, 1.2, 5)
 		print(banana)
 		for (x,y,w,h) in banana:
-			cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-			roi_color = img[y:y+h, x:x+w]
+			cv2.rectangle(cv_img,(x,y),(x+w,y+h),(255,0,0),2)
+			roi_color = cv_img[y:y+h, x:x+w]
 		
 		# Write to file
 		cv2.imwrite("./img_classified.jpg", img)
